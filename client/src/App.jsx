62a47a907fd34d1120723d950a2f446353240336
dhanashree7-ctrl <dhanashreedesai75@gmail.com>
2026-04-13 12:06:26 +0530

    feat: add responsive Navbar and Footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Login from './pages/login';
 import Register from './pages/register';
 
 function App() {
   return (
-    <Routes>
-      {/* Default path redirects to login */}
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
 
